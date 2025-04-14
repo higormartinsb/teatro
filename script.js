670999b96f9290af99a9a92540a83bee67b60c74
higormartinsb <136651293+higormartinsb@users.noreply.github.com>
2025-04-14 07:54:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-/*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) =>{
-  const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
-  toggle.addEventListener('click', () =>{
-      // Add show-menu class to nav menu
-      nav.classList.toggle('show-menu')
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
 
-      // Add show-icon to show and hide the menu icon
-      toggle.classList.toggle('show-icon')
-  })
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
 }
 
-showMenu('nav-toggle','nav-menu')
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
